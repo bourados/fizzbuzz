c29734205def7c949ae39744f8c5c1739de56c36
Radhouane <radhouane.bouazizi@dataimpact.fr>
2020-02-06 15:30:04 +0100

    implementing the statistics route

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -10,6 +9,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Input is the structure of the body of the request
 type Input struct {
 	Int1  int    `json:"int1"`
 	Int2  int    `json:"int2"`
@@ -19,11 +19,18 @@ type Input struct {
 }
 
 func main() {
+	// history contains the requests and their number of hits
+	history := make(map[string]int)
 
-	router := mux.NewRouter().StrictSlash(true)
+	router := mux.NewRouter().StrictSlash(false)
 
-	router.HandleFunc("/fizzbuzz", DoFizzBuzz).Methods("POST")
-	router.HandleFunc("/stat", DoStatics)
+	router.HandleFunc("/fizzbuzz", func(w http.ResponseWriter, r *http.Request) {
+		DoFizzBuzz(w, r, &history)
+	}).Methods("POST")
+
+	router.HandleFunc("/stat", func(w http.ResponseWriter, r *http.Request) {
+		DoStatics(w, r, &history)
+	}).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
@@ -31,7 +38,7 @@ func main() {
 // DoFizzBuzz this function Returns a list of strings with numbers from 1 to limit,
 // where: all multiples of int1 are replaced by str1, all multiples of int2 are replaced by str2,
 // all multiples of int1 and int2 are replaced by str1str2
-func DoFizzBuzz(w http.ResponseWriter, r *http.Request) {
+func DoFizzBuzz(w http.ResponseWriter, r *http.Request, history *map[string]int) {
 	var input Input
 	decoder := json.NewDecoder(r.Body)
 	decoder.Decode(&input)
@@ -58,10 +65,31 @@ func DoFizzBuzz(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
+	// add current request to history
+	curRequest := strconv.Itoa(input.Int1) + "," + strconv.Itoa(input.Int2) + "," + strconv.Itoa(input.Limit) + "," + input.Str1 + "," + input.Str2
+	if val, ok := (*history)[curRequest]; ok {
+		(*history)[curRequest] = val + 1
+	} else {
+		(*history)[curRequest] = 1
+	}
+
 	w.Write(resp)
 }
 
-// Dostatics allows users to know what the most frequent request has been
-func DoStatics(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Statics to be implemented")
+// DoStatics allows users to know what the most frequent request has been
+func DoStatics(w http.ResponseWriter, r *http.Request, history *map[string]int) {
+	if len(*history) == 0 {
+		w.Write([]byte("There is no query sent yet"))
+	} else {
+		var mostUsedParams string
+		numHits := 0
+		// loop over history to get the most used request
+		for key, val := range *history {
+			if val > numHits {
+				mostUsedParams = key
+				numHits = val
+			}
+		}
+		w.Write([]byte(mostUsedParams + " : " + strconv.Itoa(numHits) + " times"))
+	}
 }
